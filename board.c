919d63013f5a01b4965eb67fba23bb4fc5490b8f
Alan <alan82016@.gmail.com>
2015-08-17 15:06:05 -0400

    Added additional functions, to be defined in the .c files

--- board.c
+++ board.c
@@ -1,1 +1,34 @@
 #include "board.h"
+
+
+Sub_Board* sub_board_init(Sub_Board* const sub_board)
+{
+	for(int i = 0; i < 9; i++)
+	{
+		sub_board->squares[i] = 0;
+	}
+	sub_board->winner = 0;
+	return sub_board;
+}
+
+Board* board_init(Board* const board)
+{
+	board->sub_boards = malloc(board->sub_boards, sizeof(struct sub_boards) * 9);
+}
+
+Board* board_play_turn(Board* const board, int player, int sub_board_num, int x, int y);
+
+int board_find_winner(const Board* const board);
+Sub_Board* get_sub_board(const Board* const board);
+void print_board(const Board* const board);
+
+int sub_board_find_winner(const Sub_Board* const sub_board);
+Sub_Board* sub_board_play_turn(Sub_Board* const sub_board, int x, int y)
+{
+	if(i > 8 || i < 0)
+	{
+		printf("Error: value is greater than allowed value");
+	}
+	return board->sub_boards[i];
+}
+
--- board.h
+++ board.h
@@ -1,1 +1,44 @@
-a
+#ifndef BOARD_H
+#define BOARD_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Sub_Board_{
+	//0 will denote no one
+	//1 is player 1
+	//2 is player 2
+	int squares[3][3];
+	int winner;
+};
+
+
+struct Board_{
+	//an array of sub_board where 0th board  corresponds to the sub board
+	//at the top left hand corner and the 8th board corresponds to the sub
+	//board at the bottom right hand corner
+	struct Sub_Board_* sub_boards;
+};
+
+typedef struct Sub_Board_ Sub_Board;
+typedef struct Board_ Board;
+
+//Initialization functions
+Sub_Board* sub_board_init(Sub_Board* const sub_board);
+Board* board_init(Board* const board);
+
+//Board functions
+Board* board_play_turn(Board* const board, int player, int sub_board_num, int x, int y);
+int board_find_winner(const Board* const board);
+Sub_Board* get_sub_board(const Board* const board);
+void print_board(const Board* const board);
+
+
+//Sub_board functions
+int sub_board_find_winner(const Sub_Board* const sub_board);
+Sub_board* sub_board_play_turn(Sub_board* const sub_board, int x, int y);
+
+//Other functions
+
+#endif
+
